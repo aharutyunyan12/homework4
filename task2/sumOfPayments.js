487c59708ec66314db4dafc7693179c66a2b9870
Anna Harutyunyan <aharutyunyan745@gmail.com>
2024-09-08 12:44:08 +0400

    Calculate the sum of all payments

---
+++ task2/sumOfPayments.js
@@ -0,0 +1,59 @@
+const payments = [
+    {
+    studentId: 1, 
+    studentName: "John Doe",
+    payedAmount: 1600,
+    year: 2020,
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1500,
+    year: 2021
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1400,
+    year: 2022
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1500,
+    
+    year: 2020
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1400,
+    year: 2021
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1600,
+    year: 2023
+    }
+    ];
+
+function sumOfPayments (arr = []) {
+    return arr.reduce((sum, payment) => sum + payment.payedAmount, 0)
+}    
+
+const res = sumOfPayments(payments)
+
+console.log(res)
