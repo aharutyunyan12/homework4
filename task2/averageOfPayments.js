6c9d09c022eb23056c2e06621e53e9909a239e85
Anna Harutyunyan <aharutyunyan745@gmail.com>
2024-09-08 12:54:15 +0400

    Calculate the average of all payments

---
+++ task2/averageOfPayments.js
@@ -0,0 +1,58 @@
+const payments = [
+    {
+    studentId: 1, 
+    studentName: "John Doe",
+    payedAmount: 1600,
+    year: 2020,
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1500,
+    year: 2021
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1400,
+    year: 2022
+    },
+    {
+    studentId: 1,
+    studentName: "John Doe",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1500,
+    year: 2020
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1400,
+    year: 2021
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2,
+    studentName: "Lu Kang",
+    payedAmount: 1200,
+    year: 2023
+    }
+    ];
+
+function averageOfPayments (arr = []) {
+    return arr.reduce((sum, payment) => sum + payment.payedAmount, 0) / arr.length
+}
+
+const res = averageOfPayments(payments)
+
+console.log(res)
